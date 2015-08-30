d3d86c58dc8a2e29f6227e62699f32db40d27d2e
Rose <rosexu1996@yahoo.com>
2015-08-29 23:36:31 -0400

    creat post request for player name

--- app/controllers/CreateGamePage.scala
+++ app/controllers/CreateGamePage.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{AnyContent, Action, Controller}
 
 /**
  * Created by rosexu on 15-08-29.
@@ -9,5 +9,18 @@ import play.api.mvc.{Action, Controller}
 class CreateGamePage extends Controller {
   def index = Action {
     Ok(views.html.creategame())
+  }
+
+  def storeGameKey = Action(parse.tolerantFormUrlEncoded) { request =>
+    val name: String = getStringName(request.body.get("name").map(_.head))
+    println(name)
+    Ok(views.html.index())
+  }
+
+  def getStringName(option: Option[String]): String = {
+    option match {
+      case Some(name) => name
+      case None => "error"
+    }
   }
 }
